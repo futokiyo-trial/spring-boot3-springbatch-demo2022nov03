acdc00a6f691cc01d8d6242b0b5b1d20261e7fad
futokiyo <futokiyo@terra.dti.ne.jp>
2022-11-03 22:30:49 +0900

    Initial Commit

---
+++ src/main/java/io/futokiyo/springboot3springbatchdemo2022nov03/SpringBoot3SpringbatchDemo2022nov03Application.java
@@ -0,0 +1,13 @@
+package io.futokiyo.springboot3springbatchdemo2022nov03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3SpringbatchDemo2022nov03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3SpringbatchDemo2022nov03Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/futokiyo/springboot3springbatchdemo2022nov03/SpringBoot3SpringbatchDemo2022nov03ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.futokiyo.springboot3springbatchdemo2022nov03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3SpringbatchDemo2022nov03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
